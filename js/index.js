a6784297503c33d49e6350e5bb951cd795157b2a
Sainath Varanasi <varanasisai@gmail.com>
2016-05-08 16:11:55 -0400

    Minor changes for secure connection

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(function() {
-  $('<img/>').attr('src', 'http://lorempixel.com/1400/900/nature/3').load(function() {
+  $('<img/>').attr('src', 'images/background.jpg').load(function() {
     $('.bg-img').append($(this));
     // simulate loading
-    setTimeout(function() { 
-     $('.container').addClass('loaded'); 
+    setTimeout(function() {
+     $('.container').addClass('loaded');
     }, 1500)
    //$(this).remove(); // prevent memory leaks as @benweet suggested
   });
