05bd32c47c3d9de90d6dd64c4190f7f742c0a028
Sainath Varanasi <varanasisai@gmail.com>
2016-05-09 01:35:23 -0400

    upload

---
+++ black/js/index.js
@@ -0,0 +1,9 @@
+/*
+Case Study
+
+"Blackout is a cinematic experience done purely in CSS that explores the phenomenon caused by extinction of lights"
+
+https://www.behance.net/gallery/20910707/Blackout
+
+(Note: This is a just-for-fun project and it is not feasible for actual deployment as there are many performance issues. There are better technologies for similar output)
+*/
--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
-/*
-Case Study
-
-"Blackout is a cinematic experience done purely in CSS that explores the phenomenon caused by extinction of lights"
-
-https://www.behance.net/gallery/20910707/Blackout
-
-(Note: This is a just-for-fun project and it is not feasible for actual deployment as there are many performance issues. There are better technologies for similar output)
-*/
+$(function() {
+  $('<img/>').attr('src', 'images/background.jpg').load(function() {
+    $('.bg-img').append($(this));
+    // simulate loading
+    setTimeout(function() {
+     $('.container').addClass('loaded');
+    }, 1500)
+   //$(this).remove(); // prevent memory leaks as @benweet suggested
+  });
+  $('.form-toggle').on('click', function() {
+    $('.container').toggleClass('show-register')
+  })
+})
